dadf20001763d3249005e7f913fb5649eca04432
Adrian Fernandez <ing.adrian94fdz@gmail.com>
2020-02-04 15:37:26 -0600

    Cors fix

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,23 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const helmet = require('helmet')
-const routes = require('./routes/Router')
-const compression = require('compression')
-const cors = require('cors')
-const db = require('./mongoose.client')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const routes = require('./routes/Router');
+const compression = require('compression');
+const cors = require('cors');
+const db = require('./mongoose.client');
+const app = express();
 
-db()
-app.set('port', process.env.PORT || 3000)
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded( { extended: false } ))
-app.use(helmet())
-app.use(compression())
-app.use(cors())
-app.use('/api', routes)
+
+app.set('port', process.env.PORT || 3000);
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded( { extended: false } ));
+app.use(helmet());
+app.use(compression());
+app.use(cors());
+db();
+app.use('/api', routes);
 
 const port: string = app.get('port')
 app.listen(port, () => {
