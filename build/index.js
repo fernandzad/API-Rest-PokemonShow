c2aa753d4e5fccd82f809588af7e4552fa3e490b
Adrian Fernandez <ing.adrian94fdz@gmail.com>
2020-02-04 15:59:01 -0600

    Cors fix

--- build/index.js
+++ build/index.js
@@ -18,6 +18,12 @@ app.use(compression());
 app.use(cors());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Headers", 'Authorization, Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
 });
 db();
 app.use('/api', routes);
--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,15 @@ app.use(compression());
 app.use(cors());
 app.use((req: Request, res: Response, next: NextFunction) => {
     res.header("Access-Control-Allow-Headers", 'Authorization, Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+
 });
 db();
 app.use('/api', routes);
